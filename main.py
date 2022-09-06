ddc7d03d44d5ce66a064c851155d0c36d1afdc88
Arkadii <aark.lu@gmail.com>
2022-09-06 19:43:23 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-print ('         *         ******* *     *     *          *         *****      ******* ******* ' 
-       '\n''        * *        *        *    *    *          * *        *      *      *       *'
-       '\n''       *   *       *        *    *   *          *   *       *       *     *       *'
-       '\n''      *     *      *        *    *  *          *     *      *        *    *       *' 
-       '\n''     *       *     **********    * *          *       *     *        *    *       *'
-       '\n''    ***********    *        *    *   *       ***********    *        *    *       *'
-       '\n''   *           *   *         *   *    *     *           *   *       *     *       *'
-       '\n''  *             *  *          *  *     *   *             *  *      *      *       *'
-       '\n'' *               * *           * *      * *               * *****      ******* *******')
+print( '\n import antigravity \n import __hello__ \n from __future__ import braces')
+
+print('\n\t\t *\t\t   ******* *\t *\t   *\t\t  *\t\t\t*****\t   ******* ******* ' 
+       '\n\t\t* *\t\t   *\t\t*\t *\t  *\t\t\t * *\t\t*\t  *\t\t  *\t\t  *'
+       '\n\t   *   *\t   *\t\t*\t *   * \t\t\t*   *\t    *\t    *\t  *\t \t  *'
+       '\n''\t  *\t\t*\t   *\t\t*\t *  *\t\t   *\t *\t    *\t\t *\t  *\t\t  *' 
+       '\n''\t *\t     *\t   **********\t * *\t\t  *\t\t  *\t \t*\t\t *    *\t\t  *'
+       '\n''\t***********\t   *\t\t*\t *   *\t  \t ***********\t*\t\t *\t  *\t\t  *'
+       '\n''   *\t\t   *   *\t\t *   *\t  *\t \t*\t\t    *   *\t    *\t  *\t\t  *'
+       '\n''  *\t\t\t\t*  *\t\t  *  *\t   *   *\t\t\t *  *\t  *\t  \t  *\t\t  *'
+       '\n'' *\t\t  \t\t * *\t\t   * *\t    * *\t\t\t\t  * *****\t   ******* *******\n')
+
+print('Escape sequences \n \\a   Bell (alert)\n \\b   Backspace \n \\n   New line \n \\t   Horizontal tab '
+      ' \n \\\\   Backslash \  \n Double quotation mark \"  \n Single quotation mark \'')
+
+

51013a5c9a3e99263d2e612e004be6385e05e72d
Arkadii <aark.lu@gmail.com>
2022-09-09 02:19:55 +0300

    Task 2. First commit

---
+++ Task2.py
@@ -0,0 +1,13 @@
+SchoolersNmbr = int(input('Сколько школьников хотят яблоки?'"\n"))
+ApplesInBusket = int(input('Сколько сколько яблок лежит в корзине?'"\n"))
+
+print('Каждый школьник получит следующее количество яблок:',int(ApplesInBusket / SchoolersNmbr),'шт.'
+       '\n''В корзине останется:', int(ApplesInBusket % SchoolersNmbr),'шт.')
+
+print('\n''**************************************************************************')
+
+MathClaseesNubmer = 3
+DeskCapacity = 2
+SchoolersNmbrFrstClass = int(input('Сколько школьников поступят в первый класс?'"\n"))
+SchoolersNmbrScndClass = int(input('Сколько школьников поступят во второй класс?'"\n"))
+SchoolersNmbrThirdClass = int(input('Сколько школьников поступят в третий класс?'"\n"))
